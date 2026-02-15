3d18f56a4e669b875ae91173d277408c8eb554b5
gemima1509 <gemimajeniffer2210@gmail.com>
2026-02-15 22:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-
+import streamlit as st
+import numpy as np
+import joblib
+from tensorflow.keras.models import load_model
+#Load Model and Scaler
+model = load_model("backend/lstm_model.h5")
+scaler = joblib.load("backend/scaler.pkl")
+st.title("Sandrapalli's AI Investment Prediction Dashboard")
+st.write("Enter last 90 days stock values (comma separated):")
+user_input = st.text_area("Stock Data (90 values)")
+if st.button("Predict"):
+  try:
+    values = list(map(float, user_input.split(",")))
+    if len(values) != 90:
+        st.error("Please enter exactly 90 values.")
+  else:
+       data = np.array(values).reshape(-1,1)
+       scaled = scaler.transform(data)
+       scaled = scaled.reshape(1, 90, 1)
+       prediction = model.predict(scaled)
+       prediction = scaler.inverse_transform(prediction)
+       st.success(f"Predicted Next Value: {prediction[0][0]: 2f}")
+  except:
+      st.error("Invalid input. Please enter numbers separated by commas.")
