6e6da68130b83e42333b69ebd493d22a8e824bd6
vaishx17 <vaish171105@gmail.com>
2026-02-15 10:08:57 +0530

    prepare for deployment

--- backend/main.py
+++ backend/main.py
@@ -83,3 +83,6 @@ def analyze(stock: str):
         "confidence": round(confidence, 2),
         "decision": decision
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
